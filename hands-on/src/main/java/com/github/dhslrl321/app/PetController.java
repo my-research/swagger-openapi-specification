79296fd3452d976d91c9e6046e938b63395b7e6c
dhslrl321 <dhslrl321@gmail.com>
2023-05-24 00:23:01 +0900

    commit

---
+++ hands-on/src/main/java/com/github/dhslrl321/app/PetController.java
@@ -0,0 +1,12 @@
+package com.github.dhslrl321.app;
+
+import com.github.dhslrl321.openapi.PetsApi;
+import com.github.dhslrl321.openapi.invoker.ApiException;
+import com.github.dhslrl321.openapi.model.CreatePetData;
+
+public class PetController extends PetsApi {
+    @Override
+    public void createPets(CreatePetData createPetData) throws ApiException {
+
+    }
+}
